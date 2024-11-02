1266a5b940a1b3013cf53de3917467d1850a6d82
Sahil Jaiswal <sahiljaiswal550@gmail.com>
2024-11-03 01:07:11 +0530

    Created Contact Page

--- src/layout.jsx
+++ src/layout.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Navbar from "./components/navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/footer";
+import { ToastContainer } from "react-toastify";
 
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen bg-blue-50">
       <Navbar />
+      <ToastContainer position="top-center" theme="light" />
       <Outlet />
       <div className="mt-auto">
         <Footer />
